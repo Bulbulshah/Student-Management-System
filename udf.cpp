f0a42174df3d67d7a33058b3a59bc6668843ff72
Bulbulshah <bulbulgojo@gmail.com>
2025-01-16 09:34:25 +0500

    string in arry

--- udf.cpp
+++ udf.cpp
@@ -10,12 +10,30 @@ using namespace std;
 //	return;//optional in void function
 //}
 
-int add(int ,int);
+
+//{{{{{{{{{{{{functions }}}}}}}}}}}
+//int add(int ,int);
+//int main(){
+//	int result = add( 5 , 6);
+//	cout<<" Sum : "<<result <<endl;
+//	return 0;
+//}
+//int add(int a, int b){
+//	return a + b ;
+//}
+
+int checkEvenOdd(int);
 int main(){
-	int result = add( 5 , 6);
-	cout<<" Sum : "<<result <<endl;
+	int num = 6;
+	if ( checkEvenOdd(num))
+	cout<< num << " is Even "<<endl;
+	else
+	cout<< num << " is odd "<<endl;
 	return 0;
 }
-int add(int a, int b){
-	return a + b ;
+int checkEvenOdd(int num){
+	if(num % 2 == 0)
+	return 1;
+	else
+	return 0;
 }
